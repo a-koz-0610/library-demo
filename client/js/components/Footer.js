804e99d2a1cd8867c91ac6e11bb576d0485a7ed0
alan <alan@wecancodeit.org>
2021-09-08 08:57:44 -0400

    remove client side folder

--- client/js/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return `
-    <small>&copy wcci 2021</small>
-    <ul class='social-list'>
-        <li>GitHub</li>
-        <li>LinkedIn</li>
-    </ul>
-    `;
-}
